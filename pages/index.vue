6e01384678f18dee603efccd4719082b7ea74be3
fejiroofficial <38490848+fejiroofficial@users.noreply.github.com>
2021-05-16 00:04:20 +0100

    clean up

--- pages/index.vue
+++ pages/index.vue
@@ -92,14 +92,4 @@ export default {
 }
 </script>
 
-<style>
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-</style>
+<style></style>
